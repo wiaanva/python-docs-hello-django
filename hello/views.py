459d7a151ead2544dba83e2c66597b35cb662057
Willem van Aswegen <wiaanva@live.co.za>
2021-05-03 12:57:52 +0200

    Added new lines
    /n/r did not work.  Saw it using HttpResponse and determined that I must use HTTP tags (<br>)

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse ## HttpResponse uses HTML syntax.
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!\r\n \r\n This is a little attempt at preparing a home for my RymBot App service hosted on Azure.  \r\n\r\n See it as a sandbox where i am going to explore ways of satisfying my development ambitions:\n 1. To learn coding.\n 2. WatRymMet website.\n 3. Apex Legends data analytics.\n 4. Anything else I want to use Python for (Monitoring ETL pipelines, general blog of my journey (I would like to share my experiences with the world). Check back soon for more! \n ~ Wiaan, 2021/05/03")
+    return HttpResponse("Hello, World!<br><br>This is a little attempt at preparing a home for my RymBot App service hosted on Azure.  \r\n\r\n See it as a sandbox where i am going to explore ways of satisfying my development ambitions:\n 1. To learn coding.\n 2. WatRymMet website.\n 3. Apex Legends data analytics.\n 4. Anything else I want to use Python for (Monitoring ETL pipelines, general blog of my journey (I would like to share my experiences with the world). Check back soon for more! \n ~ Wiaan, 2021/05/03")
