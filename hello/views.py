349c8d2563fa454170d7b42d09814545f84d1a72
Willem van Aswegen <wiaanva@live.co.za>
2021-05-03 11:58:18 +0200

    Update Landing Page View
    Added my personal introduction.

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World!\n \n This is a little attempt at preparing a home for my RymBot App service hosted on Azure.  \n \n See it as a sandbox where i am going to explore ways of satisfying my development ambitions:\n 1. To learn coding.\n 2. WatRymMet website.\n 3. Apex Legends data analytics.\n 4. Anything else I want to use Python for (Monitoring ETL pipelines, general blog of my journey (I would like to share my experiences with the world). Check back soon for more! \n ~ Wiaan, 2021/05/03")
